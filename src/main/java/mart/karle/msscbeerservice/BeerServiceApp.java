6319b4754ffc68d1b097aac08ff8b1672f527cb8
Carlos Martínez <ingeniero.carlos.martinez@gmail.com>
2019-11-18 17:29:51 +0100

    - Includes ActiveMq Artemis auto configuration. - Adds Artemis properties defaults for this docker image - vromero/activemq-artemis. - Adds Jackson MessageConverter.

--- src/main/java/mart/karle/msscbeerservice/BeerServiceApp.java
+++ src/main/java/mart/karle/msscbeerservice/BeerServiceApp.java
@@ -2,9 +2,8 @@ package mart.karle.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class BeerServiceApp {
 
   public static void main(final String[] args) {
